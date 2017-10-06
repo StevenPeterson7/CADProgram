a24474bd82466d470870de296a9dfc4dfea34ad9
steven <steven.charles.peterson@gmail.com>
2017-10-06 12:22:13 -0700

    made dope color circle clicker thing

--- CADProgram/src/ofApp.cpp
+++ CADProgram/src/ofApp.cpp
@@ -13,9 +13,9 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	for (int i = 0; i < points.size(); i += 2) {
-		ofSetColor(rand() % 255);
-			ofDrawCircle(points[i], points[i + 1], 100, 100);
+	for (int i = 0; i < points.size(); i += 5) {
+		ofSetColor(ofColor(points[i + 2], points[i + 3], points[i + 4]));
+		ofDrawCircle(points[i], points[i + 1], 100);
 		
 	}
 
@@ -46,7 +46,14 @@ void ofApp::mouseDragged(int x, int y, int button){
 void ofApp::mousePressed(int x, int y, int button){
 	points.push_back(x);
 	points.push_back(y);
-	cout << x << "," << y << endl;
+	int r = rand() % 255;
+	int g = rand() % 255;
+	int b = rand() % 255;
+	points.push_back(r);
+	points.push_back(g);
+	points.push_back(b);
+
+	cout << r << ", " << g << ", " << b << endl;
 
 }
 
